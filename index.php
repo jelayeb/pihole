50dea3bc154bc8c3dca49f1e5b9245d82bf34a39
jelayeb <jasem.elayeb+github@gmail.com>
2017-02-07 12:13:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 <body>
 <center> 
 <p>
-<img src="/pihole/log3-4.png" alt="Blocked By HTI ISP" >
+<img src="/pihole/log3-4.png" alt="Blocked By Your ISP" >
 </p>
 <p>
-This Website has been blocked by your Internet Provider please contact <a href="mailto:tip.cs@hti.ly?subject=Blocked Website Report" target="_top">Support</a> if you feel this page should not be blocked.
+This Website has been blocked by your Internet Provider please contact <a href="mailto:support@domain.com?subject=Blocked Website Report" target="_top">Support</a> if you feel this page should not be blocked.
 </p>
 <p> If you are an admin <a href="/pihole/admin.php"> Login</a> here. </p>
 </center>
