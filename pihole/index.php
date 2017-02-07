a6eabc0c7a0d754a74eb63d871cfd2aa5daa3894
root <root@ns1.itech.ly>
2017-02-07 13:21:51 +0100

    add block page forward

--- pihole/index.php
+++ pihole/index.php
@@ -1,1 +1,14 @@
-test
+<html>
+
+<body>
+<center> 
+<p>
+<img src="http://itech.ly/iTech_files/itech-modernsolutions3-filtered.png" alt="Blocked By Your ISP" >
+</p>
+<p>
+This Website has been blocked by your Internet Provider please contact <a href="mailto:support@domain.com?subject=Blocked Website Report" target="_top">Support</a> if you feel this page should not be blocked.
+</p>
+<p> If you are an admin <a href="/pihole/admin.php"> Login</a> here. </p>
+</center>
+</body>
+</html>
