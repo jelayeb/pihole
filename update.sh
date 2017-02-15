19a6a3bf6b46a27b706d9ee1f406b83e69b81a33
root <root@ns1.itech.ly>
2017-02-15 17:13:47 +0100

    add install pihole_update.sh

--- update.sh
+++ update.sh
@@ -7,7 +7,9 @@ cp -r ./manage/data /var/www/html/admin/manage/
 chown -R lighttpd /var/www/html/admin/manage/data
 chown lighttpd /etc/pihole/adlists.list
 
-
+cp ./manage/pihole_update.sh /var/www/cgi-bin/
+chown /var/www/cgi-bin/pihole_update.sh
+chmod +x /var/www/cgi-bin/pihole_update.sh
 
 echo "lighttpd ALL=NOPASSWD: /usr/bin/rm -f /etc/pihole/list.*127.0.0.1*.domains" >> /etc/sudoers.d/pihole
 echo "lighttpd ALL=NOPASSWD: /usr/bin/dos2unix -q /etc/pihole/adlists.list" >> /etc/sudoers.d/pihole
