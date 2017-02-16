4f75bda4821e256c56a9e34c7a990c8be4df9436
root <root@ns1.itech.ly>
2017-02-16 16:15:15 +0100

    copy replace header.php

--- update.sh
+++ update.sh
@@ -11,5 +11,8 @@ cp ./manage/pihole_update.sh /var/www/cgi-bin/
 chown lighttpd /var/www/cgi-bin/pihole_update.sh
 chmod +x /var/www/cgi-bin/pihole_update.sh
 
+mv /var/www/html/admin/scripts/pi-hole/php/header.php /var/www/html/admin/scripts/pi-hole/php/header.php.org
+cp ./manage/header.php /var/www/html/admin/scripts/pi-hole/php/
+
 echo "lighttpd ALL=NOPASSWD: /usr/bin/rm -f /etc/pihole/list.*127.0.0.1*.domains" >> /etc/sudoers.d/pihole
 echo "lighttpd ALL=NOPASSWD: /usr/bin/dos2unix -q /etc/pihole/adlists.list" >> /etc/sudoers.d/pihole
