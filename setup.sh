3db596f3af4f6181554101a9b3378183f561f42f
jelayeb <jasem.elayeb+github@gmail.com>
2017-02-24 10:53:19 +0100

    Create setup.sh to replace the install.sh

---
+++ setup.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+# Ver=0.9
+# Move to pihole-cat with new interface
+# clean up old installation and create backup before proceed. 
+
+NOW="$(date +"%d%m%Y-%H:%M")"
+
+which pihole &> /dev/null
+[ $? -ne 0 ] && echo -e " \e[31mError:\e[0m  PiHole is not Installed on the system,  install Pihole first then proceed with install.sh again.." && exit 1
+
+echo -e " \e[32mOK:\e[0m PiHole is installed, Proceed with Setup....  "
+
+if [ -d "/var/www/html/admin" ]
+then
+    echo -e " \e[32mOK:\e[0m PiHole Admin Structure exist, proceed with installation.." 
+
+	if [ -d "/var/www/html/pihole-cat"]
+	then
+		echo -e " \e[33mWarning:\e[0m PiHole-Cat Structure exist, proceed with Backup..."
+		tar -czvf /var/www/html/pihole-cat.${NOW}.tar.gz --remove-files /var/www/html/pihole-cat /var/www/cgi-bin/*.sh
+		chown root.root /var/www/html/pihole-cat.*
+		chmod -o /var/www/html/pihole-cat.*
+		cp -Rv pihole-cat /var/www/html/
+		chown -R lighttpd:lighttpd /var/www/html/pihole-cat
+		chmod -R 755 /var/www/html/pihole-cat
+		cp -v pihole-cat/manage/*.sh /var/www/cgi-bin/
+		chmod 755 /var/www/cgi-bin/pihole*.sh
+		chown lighttpd /var/www/cgi-bin/pihole*.sh
+		
+	else
+		cp -Rv pihole-cat /var/www/html/
+		chown -R lighttpd:lighttpd /var/www/html/pihole-cat
+		chmod -R 755 /var/www/html/pihole-cat
+		cp -v pihole-cat/manage/*.sh /var/www/cgi-bin/
+		chmod 755 /var/www/cgi-bin/pihole*.sh
+		chown lighttpd /var/www/cgi-bin/pihole*.sh
+
+	fi
+
+
+	cat ./pihole-cat/manage/categories/custom.list >> /etc/pihole/adlists.list
+	chown lighttpd /etc/pihole/adlists.list
+	/usr/bin/dos2unix -q /etc/pihole/adlists.list
+	/usr/bin/rm -f /etc/pihole/list.*127.0.0.1*.domains
+	chmod 755 /var/www/html/pihole-cat/manage/categories
+		
+	CGI_CONF=/etc/lighttpd/conf.d/cgi.conf
+
+	if [ -f ${CGI_CONF} ]
+	then
+		CGI_UPDATE=`cat <<EOCGI
+		alias.url += ( "/cgi-bin" => server_root + "/cgi-bin" )
+		$HTTP["url"] =~ "^/cgi-bin" {
+		cgi.assign = ( ".sh" => "/usr/bin/sh" )
+		}
+		EOCGI
+		`
+		echo -e ${CGI_UPDATE} >> ${CGI_CONF}
+	
+	else
+	
+	echo -e " \e[31mError:\e[0m ${CGI_CONF} is not exist, check lighttpd setup, and try again.."
+	exit 1
+	
+	fi
+	
+else
+    echo -e " \e[31mError:\e[0m PiHole Admin structure is not exist, please check pihole installation and setup again..."
+    exit 1
+fi
